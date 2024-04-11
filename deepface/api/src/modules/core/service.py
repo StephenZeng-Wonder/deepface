695ecac0f2b369b88be318f07c81d8053a8e8868
Stephen <stephenzeng@xm.wonder.com>
2024-04-11 02:08:35 -0400

    add find api

--- deepface/api/src/modules/core/service.py
+++ deepface/api/src/modules/core/service.py
@@ -13,7 +13,12 @@ def find(img_path, db_path, model_name, detector_backend, enforce_detection, ali
             enforce_detection=enforce_detection,
             align=align,
         )
-        result["results"] = similarities
+        rst = []
+        for s in similarities:
+            e = {"identity": s.identity, "distance": s.distance, "threshold": s.threshold, "target_x": s.target_x,
+                 "target_y": s.target_y, "target_w": s.target_w, "target_h": s.target_h}
+            rst.append(e)
+        result["results"] = rst
         return result
     except Exception as err:
         return {"error": f"Exception while finding: {str(err)}"}, 400
